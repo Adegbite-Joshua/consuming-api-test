1f6f2174d4b9a86afb94d10a0081b899ded84793
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-05-19 11:26:18 +0100

    Performing live coding test

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-let response = ''
+let response = {
+    riddle: '',
+    answer: ''
+}
 const fetchRiddle =()=>{
-let endpoint = 'https://riddles-api.vercel.app/random'
+    let endpoint = 'https://riddles-api.vercel.app/random'
     fetch(endpoint)
     .then((res)=>res.json())
     .then((res)=>{
         console.log(res);
-        // response = res.data.ri/ddle
+        document.getElementById('riddleDisplay').innerHTML = res.riddle
+        document.getElementById('answer').innerHTML = res.answer
     })
     .catch((err)=>{
         console.log(err);
     })
+}
+const showAnswer=()=>{
+    console.log(document.getElementById('answer'));
+    document.getElementById('answer').style.display = 'block'
 }
 
 window.onload = fetchRiddle()
